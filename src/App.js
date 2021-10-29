c7ef16763b6bf569f8ef7929ab426ce077974375
Elisabet D'Acosta <venganzaalchocolate@gmail.com>
2021-10-29 13:22:03 +0200

    creando grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './footer/Footer';
+import Header from './header/Header';
+import Contenido from './section/Contenido';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="grid">
+            <Header />
+            <Contenido/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/section/Contenido.js
@@ -0,0 +1,7 @@
+import './Contenido.css';
+
+function Contenido() {
+    return <section className="contenido">contenido</section>;
+}
+
+export default Contenido;
